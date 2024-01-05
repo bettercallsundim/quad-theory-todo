fbbd210c1ea9f6ee4e8d40517ad9f82c8d555a7b
sunxim <sunmaxboom2@gmail.com>
2024-01-05 21:16:01 +0600

    first commit

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,7 +1,7 @@
 import { FaGoogle, FaInstagram, FaTwitter } from "react-icons/fa";
 import { FaArrowRightLong } from "react-icons/fa6";
 
-export default Footer = () => {
+export default function Footer() {
   return (
     <footer className="  bg-[#F99F1C] mt-20 md:py-20  px-4 py-12 md:px-32 flex items-center justify-around">
       <div className="first w-full md:w-[50%]">
@@ -56,4 +56,4 @@ export default Footer = () => {
       </div>
     </footer>
   );
-};
+}
